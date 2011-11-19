fd5dba24d13e0cd25bee9dccc3566dc9c889b01d
Jake Trent <trent.jake@gmail.com>
2011-11-18 19:31:06 -0700

    Fix static file serve, at least locally

--- rockycode/urls.py
+++ rockycode/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -18,3 +19,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
