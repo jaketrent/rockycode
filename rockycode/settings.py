ca84429761d44f1a79d74dafd414ee69522169a6
Jake Trent <trent.jake@gmail.com>
2011-11-18 23:04:17 -0700

    Host statics on dropbox cdn

--- rockycode/settings.py
+++ rockycode/settings.py
@@ -58,11 +58,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''#os.path.join(PROJ_PATH, "static")
+STATIC_ROOT = os.path.join(PROJ_PATH, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://dl.dropbox.com/u/990505/rockycode-static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -74,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJ_PATH, "static"),
+    os.path.join(PROJ_PATH, "static-src"),
 )
 
 # List of finder classes that know how to find static files in
--- rockycode/urls.py
+++ rockycode/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -20,4 +20,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += staticfiles_urlpatterns()
