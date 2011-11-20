070f69687ae4e71ade41fce85dbcf0611fd2e320
Jake Trent <trent.jake@gmail.com>
2011-11-20 10:19:44 -0700

    New rst pygments directive added

--- rockycode/blog/__init__.py
+++ rockycode/blog/__init__.py
@@ -1,39 +1,38 @@
-__version__ = '1.8.1a'
-
+# -*- coding: utf-8 -*-
 """
-The Pygments reStructuredText directive
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    The Pygments reStructuredText directive
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-This fragment is a Docutils_ 0.4 directive that renders source code
-(to HTML only, currently) via Pygments.
+    This fragment is a Docutils_ 0.5 directive that renders source code
+    (to HTML only, currently) via Pygments.
 
-To use it, adjust the options below and copy the code into a module
-that you import on initialization.  The code then automatically
-registers a ``sourcecode`` directive that you can use instead of
-normal code blocks like this::
+    To use it, adjust the options below and copy the code into a module
+    that you import on initialization.  The code then automatically
+    registers a ``sourcecode`` directive that you can use instead of
+    normal code blocks like this::
 
-    .. sourcecode:: python
+        .. sourcecode:: python
 
-        My code goes here.
+            My code goes here.
 
-If you want to have different code styles, e.g. one with line numbers
-and one without, add formatters with their names in the VARIANTS dict
-below.  You can invoke them instead of the DEFAULT one by using a
-directive option::
+    If you want to have different code styles, e.g. one with line numbers
+    and one without, add formatters with their names in the VARIANTS dict
+    below.  You can invoke them instead of the DEFAULT one by using a
+    directive option::
 
-    .. sourcecode:: python
-        :linenos:
+        .. sourcecode:: python
+            :linenos:
 
-        My code goes here.
+            My code goes here.
 
-Look at the `directive documentation`_ to get all the gory details.
+    Look at the `directive documentation`_ to get all the gory details.
 
-.. _Docutils: http://docutils.sf.net/
-.. _directive documentation:
-    http://docutils.sourceforge.net/docs/howto/rst-directives.html
+    .. _Docutils: http://docutils.sf.net/
+    .. _directive documentation:
+       http://docutils.sourceforge.net/docs/howto/rst-directives.html
 
-:copyright: 2007 by Georg Brandl.
-:license: BSD, see LICENSE for more details.
+    :copyright: Copyright 2006-2011 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
 """
 
 # Options
@@ -42,49 +41,42 @@ Look at the `directive documentation`_ to get all the gory details.
 # Set to True if you want inline CSS styles instead of classes
 INLINESTYLES = False
 
-try:
-    from pygments.formatters import HtmlFormatter
+from pygments.formatters import HtmlFormatter
 
-    # The default formatter
-    DEFAULT = HtmlFormatter(noclasses=INLINESTYLES)
+# The default formatter
+DEFAULT = HtmlFormatter(noclasses=INLINESTYLES)
 
-    # Add name -> formatter pairs for every variant you want to use
-    VARIANTS = {
-        'linenos': HtmlFormatter(noclasses=INLINESTYLES, linenos=True),
-    }
+# Add name -> formatter pairs for every variant you want to use
+VARIANTS = {
+    # 'linenos': HtmlFormatter(noclasses=INLINESTYLES, linenos=True),
+}
 
-    from docutils import nodes
-    from docutils.parsers.rst import directives
 
-    from pygments import highlight
-    from pygments.lexers import get_lexer_by_name, TextLexer
+from docutils import nodes
+from docutils.parsers.rst import directives, Directive
 
-    def pygments_directive(name, arguments, options, content, lineno,
-                        content_offset, block_text, state, state_machine):
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, TextLexer
+
+class Pygments(Directive):
+    """ Source code syntax hightlighting.
+    """
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = dict([(key, directives.flag) for key in VARIANTS])
+    has_content = True
+
+    def run(self):
+        self.assert_has_content()
         try:
-            lexer = get_lexer_by_name(arguments[0])
+            lexer = get_lexer_by_name(self.arguments[0])
         except ValueError:
             # no lexer found - use the text one instead of an exception
-            print "DEFAULTED TO TEXTLEXER"
             lexer = TextLexer()
         # take an arbitrary option if more than one is given
-        formatter = options and VARIANTS[options.keys()[0]] or DEFAULT
-        parsed = highlight(u'\n'.join(content), lexer, formatter)
-        parsed = '<div class="codeblock">%s</div>' % parsed
+        formatter = self.options and VARIANTS[self.options.keys()[0]] or DEFAULT
+        parsed = highlight(u'\n'.join(self.content), lexer, formatter)
         return [nodes.raw('', parsed, format='html')]
 
-    pygments_directive.arguments = (1, 0, 1)
-    pygments_directive.content = 1
-    pygments_directive.options = dict([(key, directives.flag) for key in VARIANTS])
-
-    directives.register_directive('sourcecode', pygments_directive)
-
-    # create an alias, so we can use it with rst2pdf... leave the other for
-    # backwards compatibility
-    directives.register_directive('code-block', pygments_directive)
-except:
-    # the user probably doesn't have pygments installed
-    pass
-
-
-
+directives.register_directive('sourcecode', Pygments)
