9ebf90e79a9bfef19a0227c44e13ff2379e71e66
Jake Trent <trent.jake@gmail.com>
2011-11-20 10:30:11 -0700

    Added static file serving diff based on DEBUG.  Debug back off

--- rockycode/settings.py
+++ rockycode/settings.py
@@ -2,7 +2,7 @@ import os
 
 PROJ_PATH = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -59,11 +59,12 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJ_PATH, "static")
+STATIC_ROOT = '' #os.path.join(PROJ_PATH, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://dl.dropbox.com/u/990505/rockycode-static/'
+
+STATIC_URL = '/static/' if DEBUG else'http://dl.dropbox.com/u/990505/rockycode-static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -75,7 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJ_PATH, "static-src"),
+    os.path.join(PROJ_PATH, "static"),
 )
 
 # List of finder classes that know how to find static files in
