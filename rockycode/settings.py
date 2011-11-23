72847fa2bad8762256809461d2ed96843bacdd1e
Jake Trent <trent.jake@gmail.com>
2011-11-22 22:10:49 -0700

    Protect sendgrid opts when running local w/o sendgrid

--- rockycode/settings.py
+++ rockycode/settings.py
@@ -163,9 +163,12 @@ MAX_TAG_LENGTH=20
 
 UPLOAD_PATH="files/"
 
-
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+try:
+  if os.environ.has_key('SENDGRID_USERNAME') and os.environ.has_key('SENDGRID_PASSWORD'):
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+except:
+    print "Mail settings error:", sys.exc_info()
