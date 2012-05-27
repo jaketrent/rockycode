4f13644cd9caba1660b10438b3852a8fd6026810
Jake Trent <trent.jake@gmail.com>
2012-05-27 10:41:11 -0600

    20 - fix autocomplete path in prod

--- rockycode/settings.py
+++ rockycode/settings.py
@@ -165,7 +165,7 @@ MAX_TAG_LENGTH=20
 
 UPLOAD_PATH="files/"
 SITE_START_DATE = date(2008, 8, 26)
-TAGGING_AUTOCOMPLETE_JS_BASE_URL = STATIC_URL + "jquery-autocomplete"
+TAGGING_AUTOCOMPLETE_JS_BASE_URL = '/static/jquery-autocomplete/' if DEBUG else 'http://dl.dropbox.com/u/990505/rockycode-static/jquery-autocomplete/'
 
 try:
   if os.environ.has_key('SENDGRID_USERNAME') and os.environ.has_key('SENDGRID_PASSWORD'):
