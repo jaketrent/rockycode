76a5c28e159bba19b8fd9a6d1e6f2972c71b82fc
jtsnake <trent.jake@gmail.com>
2012-08-23 21:48:10 -0600

    make site host own static files; use collectstatic

--- rockycode/settings.py
+++ rockycode/settings.py
@@ -3,7 +3,7 @@ from datetime import date
 
 PROJ_PATH = os.path.dirname(__file__)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -60,24 +60,19 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '' #os.path.join(PROJ_PATH, "static")
+STATIC_ROOT = os.path.join(PROJ_PATH, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 
-STATIC_URL = '/static/' if DEBUG else 'http://dl.dropbox.com/u/990505/rockycode-static/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJ_PATH, "static"),
+    os.path.join(PROJ_PATH, "static-files"),
 )
 
 # List of finder classes that know how to find static files in
--- rockycode/urls.py
+++ rockycode/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rockycode.blog.models import ArticleSitemap
-from rockycode.settings import STATIC_URL
+from rockycode import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,15 +12,11 @@ sitemaps = {
 
 urlpatterns = patterns('',
     (r'^', include('rockycode.blog.urls')),
-    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '%simages/favicon.ico' % STATIC_URL}),
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '%simages/favicon.ico' % settings.STATIC_URL}),
     (r'^tagging_autocomplete/', include('rockycode.tagging_autocomplete.urls')),
     (r'^profile/', include('profiles.urls'), {'success_url': '/profile/edit/'}),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
-
-    # Examples:
-    # url(r'^$', 'rockycode.views.home', name='home'),
-    # url(r'^rockycode/', include('rockycode.foo.urls')),
 
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}, name="sitemap"),
 
@@ -30,3 +26,8 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG is False:
+  urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+  )
